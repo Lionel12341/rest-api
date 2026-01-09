0ad986e32d9791c48f4f3562f7835901e91d4355
Lionel Raffa Radiyenaf <lionel.raffa007@gmail.com>
2026-01-09 23:12:22 +0700

    Add ecpsystem.config.js for API service configuration

---
+++ ecpsystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [{
+    name: "api-service",
+    script: "index.js",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "300M",
+    env: {
+      NODE_ENV: "production"
+    }
+  }]
+}
