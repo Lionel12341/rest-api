24b8b59d964f48271d8802c7206151479ae9a5d3
Lionel Raffa Radiyenaf <lionel.raffa007@gmail.com>
2026-01-09 23:10:53 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config()
+const express = require("express")
+const routes = require("./routes")
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(routes)
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "ok",
+    uptime: process.uptime()
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`API running on port ${PORT}`)
+})
